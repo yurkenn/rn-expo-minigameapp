48c0a72ef434df186ed6ae23e7c384645b258171
oguz yurken <oguz.yurken@gmail.com>
2022-08-28 05:35:20 +0300

    Create Screens

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const StartGameScreen = () => {
+  return (
+    <View>
+      <Text>StartGameScreen</Text>
+    </View>
+  );
+};
+
+export default StartGameScreen;
